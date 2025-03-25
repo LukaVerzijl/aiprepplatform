68f6c2621e3ca0ca72829298f40d459c1404498c
Luka Verzijl <lukaverzijl@hotmail.com>
2025-03-25 16:16:11 +0100

    before cards

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
   subsets: ["latin"],
 });
-
 
 export const metadata: Metadata = {
   title: "Prepwise",
@@ -20,10 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={"dark"}>
-      <body
-        className={`${monaSans.variable} antialiased`}
-      >
+      <body className={`${monaSans.variable} antialiased pattern`}>
         {children}
+        <Toaster />
       </body>
     </html>
   );
