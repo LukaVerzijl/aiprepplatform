cc880c69d9866135dc9943d90f54d960e5fdf14a
Luka <redlucraft@gmail.com>
2025-03-29 11:11:29 +0100

    Update route.ts

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -25,9 +25,9 @@ export async function POST(request: Request) {
     });
 
     const interview = {
-      role,
-      type,
-      level,
+      role: role,
+      type: type,
+      level: level,
       techstack: techstack ? techstack.split(",") : [],
       questions: JSON.parse(questions),
       userId: userid,
